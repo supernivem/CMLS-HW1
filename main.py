c19628f7edb032f44e5225d556291cd690f62bb6
GuidoElli <guido.elli.97@gmail.com>
2020-04-27 20:34:47 +0200

    s

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import librosa
+import scipy as sp
+
+
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+from sklearn.mixture import BayesianGaussianMixture
+import scipy.stats
+import seaborn as sns
+
