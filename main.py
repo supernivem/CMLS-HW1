45f2fdbe1007a1c9fdbd8d54225b6af9a52b0f99
GuidoElli <guido.elli.97@gmail.com>
2020-05-04 10:56:04 +0200

    v

--- main.py
+++ main.py
@@ -1,26 +1,65 @@
 import numpy as np
 import librosa
 import scipy as sp
-import os
-from feature_computation import *
 import matplotlib.pyplot as plt
 import IPython.display as ipd
 from sklearn.mixture import BayesianGaussianMixture
 import scipy.stats
 import seaborn as sns
 
-instruments = ['BM', 'GM', 'GP']
-classes = ['NOFX', 'TREM', 'DIST']
+import os
+import sklearn
 
-features = ['ZCR', 'SpecDec']
+from parameters import *
+from features import *
+from metrics import *
 
+# Compute features and labels
+x_train, y_train, x_test, y_test = [], [], [], []
 for inst in instruments:
 	for cls in classes:
 		root = 'Data/{}/{}/'.format(inst, cls)
-		files = [f for f in os.listdir(files) if f.endswith('.wav')]
-		for file_index, file in enumerate(files):
-			audio, fs = librosa.load(os.path.join(root, file), sr=None)
-			temp_features = np.zeros(len(features))
-			for feat_index, feat in enumerate(features):
-				temp_features[feat_index] = compute_feature(feat, audio, fs)
+		files = [f for f in os.listdir(root) if f.endswith('.wav')]
+		n_files = len(files)
+		last_file_excluded = np.floor(n_files * data_proportion)
+		last_train_file_excluded = np.floor(n_files * data_proportion * (1 - test_proportion))
 
+		for file_index, file in enumerate(files):
+
+			if file_index < last_file_excluded:
+				audio, fs = librosa.load(os.path.join(root, file), sr=None)
+				temp_features = compute_features(features, audio, fs)
+
+				if file_index < last_train_file_excluded:
+					x_train.append(temp_features)
+					y_train.append(cls)
+				else:
+					x_test.append(temp_features)
+					y_test.append(cls)
+
+x_train = np.array(x_train)
+x_train.reshape(-1, n_features)
+x_test = np.array(x_test)
+x_test.reshape(-1, n_features)
+y_train = np.array(y_train)
+y_train.reshape(-1, 1)
+y_test = np.array(y_test)
+y_test.reshape(-1, 1)
+
+# Normalization
+feat_max = np.max(x_train, axis=0)
+feat_min = np.min(x_train, axis=0)
+x_train_norm = (x_train - feat_min) / (feat_max - feat_min)
+x_test_norm = (x_test - feat_min) / (feat_max - feat_min)
+
+# Training
+clf = sklearn.svm.SVC(class_weight='balanced')
+clf.fit(x_train_norm, y_train)
+
+# Predict
+y_test_predicted = clf.predict(x_test_norm)
+
+# Metrics
+cm = compute_cm(y_test, y_test_predicted)
+# compute_metrics(y_test, y_test_predicted)
+print(cm)
---
+++ metrics.py
@@ -0,0 +1,34 @@
+import numpy as np
+import librosa
+import scipy as sp
+
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+from sklearn.mixture import BayesianGaussianMixture
+import scipy.stats
+import seaborn as sns
+
+from parameters import *
+
+
+def compute_metrics(gt_labels, predicted_labels):
+	tp = np.sum(np.logical_and(predicted_labels == 1, gt_labels == 1))
+	fp = np.sum(np.logical_and(predicted_labels == 1, gt_labels == 0))
+	tn = np.sum(np.logical_and(predicted_labels == 0, gt_labels == 0))
+	fn = np.sum(np.logical_and(predicted_labels == 0, gt_labels == 1))
+	accuracy = (tp + tn) / (tp + fp + tn + fn)
+	precision = tp / (tp + fp)
+	recall = tp / (tp + fn)
+	f1_score = 2 * precision * recall / (precision + recall)
+	print("Results : \n Accuracy = {} \n Precision = {} \n Recall = {} \n F1 f1_score = {}".format(
+		accuracy, precision, recall, f1_score))
+
+
+def compute_cm(real, predicted):
+	cm = np.zeros((len(classes), len(classes)))
+
+	for c in classes:
+		pred_class = predicted[real == c]
+		for d in pred_class:
+			cm[classes.index(c), classes.index(d)] += 1
+	return cm
